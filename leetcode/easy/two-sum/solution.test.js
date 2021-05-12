87fbe5954c07eb3671dc4171a5e41f6be4bdda25
saseungmin <dbd02169@naver.com>
2021-05-13 01:09:34 +0900

    [Update] Solve twoSum in leetCode

---
+++ leetcode/easy/two-sum/solution.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable consistent-return */
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+describe('twoSum', () => {
+  it('solution', () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+    expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+  });
+});
